02e86ffac088a1f6073154e07bbe6513b06158ad
Ondrej Bartas <ondrej@bartas.cz>
2015-03-12 10:45:50 +0100

    Add Dummy application

---
+++ test/dummy/config/initializers/translation_engine.rb
@@ -0,0 +1,7 @@
+TranslationEngine.config do |config|
+  config.api_key  = '4e75310553e4523c0e015606343e01c5'
+  config.api_host = 'http://127.0.0.1:3000'
+  config.use_screenshots  = true
+  config.use_catcher      = true
+  config.raise_exceptions = true
+end
